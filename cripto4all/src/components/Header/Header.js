71bd7e35a8acf1a2684c976cb6d01e29bc955385
mbiga <mbigaram.mb@gmail.com>
2023-01-29 22:27:53 -0300

    construção do header 1

---
+++ cripto4all/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                Header
+            </header>
+        </div>
+    )
+}
+
+export default Header
